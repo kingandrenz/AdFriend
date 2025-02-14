fda8f07c9d9f417c895d2c0fb070891c4436a122
flexteck <flexteckse@gmail.com>
2025-02-14 14:25:54 +0100

    integerated skip ad for youtube

--- content/content.js
+++ content/content.js
@@ -46,5 +46,35 @@ async function getRandomWidgetContent() {
   }
 }
 
+// youtub skipAd
+function skipAd() {
+  // getting ths video element and the skip button that youtube renders
+  const videoElement = document.querySelector("video");
+  const skipButton = document.querySelector("button.ytp-ad-skip-button-modern");
+
+  // move current video to the end and click the skip button
+  if (videoElement && isFinite(videoElement.duration)) {
+    videoElement.currentTime = videoElement.duration;
+  }
+
+  if (skipButton) {
+    skipButton.click();
+  }
+}
+
+function main() {
+  // listen to message fro chrome extention Api
+
+  chrome.runtime.onMessage.addListener(
+    async (request, sender, sendResponse) => {
+      if (request.action === "skip") {
+        skipAd();
+      }
+    }
+  );
+}
+
+main();
+
 // Wait until the page is fully loaded before running replaceAds
 window.addEventListener("load", replaceAds);
